a94ffc78ddd2cfef4e44a10fb8cea12db1b70986
Stephen Pangburn <stephen.pangburn.ii@gmail.com>
2017-07-18 10:27:09 -0700

    Finish user and session controllers

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,33 @@
 class Api::SessionsController < ApplicationController
+
+  def new; end
+
+  def create
+    user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if user
+      login!(user)
+      render "api/users/show"
+    else
+      render json: ["Invalid login credentials"], status: 401
+    end
+  end
+
+  def destroy
+    if current_user
+      logout!
+      render "api/users/show"
+    else
+      render json: ["User is not signed in"], status: 404
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:username, :password)
+  end
 end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,35 @@
 class Api::UsersController < ApplicationController
+
+  def new; end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login!(@user)
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  def edit
+    @user = User.find_by_id(params[:id])
+  end
+
+  def update
+    @user = User.find_by_id(params[:id])
+
+    if @user.update_attributes(user_params)
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
