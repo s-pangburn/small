7a2b488638c3225588a92aa9bfae6ffc279fb995
Stephen Pangburn <stephen.pangburn.ii@gmail.com>
2017-07-18 14:43:12 -0700

    Add store

--- frontend/small.jsx
+++ frontend/small.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
+import configureStore from './store/store';
 
-import { signup, login, logout } from './util/session_api_util';
+import { signup, login, logout } from './actions/session_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
-  ReactDOM.render(<Root/>, root);
+  const store = createStore();
+  ReactDOM.render(<Root store={store}/>, root);
 
-  window.signup = signup;
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   window.login = login;
   window.logout = logout;
 });
+
+const createStore = () => {
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+  return store;
+};
