c64882bdf4c7b1f6dd3f588715ea1c6e9ac0ad4f
Stephen Pangburn <stephen.pangburn.ii@gmail.com>
2017-07-18 14:18:49 -0700

    Finish session actions

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,38 @@
+import * as APIUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_ERRORS = 'RECEIVE ERRORS';
+
+export const receiveCurrentUser = user => ({
+  type: RECEIVE_CURRENT_USER,
+  user
+});
+
+export const receiveErrors = errors => ({
+  type: RECEIVE_ERRORS,
+  errors
+});
+
+export const signup = user => dispatch => (
+  APIUtil.signup(user).then( currentUser => (
+    dispatch(receiveCurrentUser(currentUser))
+  ), errors => (
+    dispatch(receiveErrors(errors.responseJSON))
+  ))
+);
+
+export const login = user => dispatch => (
+  APIUtil.login(user).then( currentUser => (
+    dispatch(receiveCurrentUser(currentUser))
+  ), errors => (
+    dispatch(receiveErrors(errors.responseJSON))
+  ))
+);
+
+export const logout = () => dispatch => (
+  APIUtil.logout().then( currentUser => (
+    dispatch(receiveCurrentUser(null))
+  ), errors => (
+    dispatch(receiveErrors(errors.responseJSON))
+  ))
+);
