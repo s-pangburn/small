b2e53e4681f08929627b8c565541eea40795e285
Stephen Pangburn <stephen.pangburn.ii@gmail.com>
2017-07-18 14:07:46 -0700

    Debug sessions controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -3,13 +3,13 @@ class Api::SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by_credentials(
+    @user = User.find_by_credentials(
       params[:user][:username],
       params[:user][:password]
     )
 
-    if user
-      login!(user)
+    if @user
+      login!(@user)
       render "api/users/show"
     else
       render json: ["Invalid login credentials"], status: 401
@@ -17,7 +17,8 @@ class Api::SessionsController < ApplicationController
   end
 
   def destroy
-    if current_user
+    @user = current_user
+    if @user
       logout!
       render "api/users/show"
     else
--- frontend/small.jsx
+++ frontend/small.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
 
+import { signup, login, logout } from './util/session_api_util';
+
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
   ReactDOM.render(<Root/>, root);
+
+  window.signup = signup;
+  window.login = login;
+  window.logout = logout;
 });
