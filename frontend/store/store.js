757aee84f943434cc61240afe7314fbce8cc40c8
Stephen Pangburn <stephen.pangburn.ii@gmail.com>
2017-07-29 12:01:21 -0700

    Prepare config files for production

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,11 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
-
 import rootReducer from '../reducers/root_reducer';
 
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  middlewares.push(createLogger());
+}
+
 const configureStore = (preloadedState = {}) => (
-  createStore(rootReducer, preloadedState, applyMiddleware(logger, thunk))
+  createStore(rootReducer, preloadedState, applyMiddleware(...middlewares))
 );
 
 export default configureStore;
