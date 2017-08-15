c6f32eca350c7d3dbcb1724bb38f552dfa37cb2f
Stephen Pangburn II <stephen.pangburn.ii@gmail.com>
2017-08-15 15:05:55 -0700

    Change story styling to more closely resemble Medium, add like actions

---
+++ frontend/actions/like_actions.js
@@ -0,0 +1,5 @@
+import * as APIUtil from '../util/like_api_util';
+import { receiveErrors } from './error_actions';
+
+export const RECEIVE_LIKE = "RECEIVE_LIKE";
+export const REMOVE_LIKE = "REMOVE_LIKE";
