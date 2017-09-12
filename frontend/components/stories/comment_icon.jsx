3f781f2d0a3460679e560826bcbbd6237db45c83
Stephen Pangburn II <stephen.pangburn.ii@gmail.com>
2017-09-12 10:19:42 -0700

    Comment icon now functional

--- frontend/components/stories/comment_icon.jsx
+++ frontend/components/stories/comment_icon.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
 class CommentIcon extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    window.scrollTo(0, document.getElementById('body').clientHeight + 300);
+  }
+
   render() {
     return (
-      <img
-        width="32"
-        src="http://res.cloudinary.com/dzeqeo9b3/image/upload/v1505191447/icon-comment_mg6xzj.png" />
+      <div onClick={this.handleClick}>
+        <img
+          width="32"
+          src="http://res.cloudinary.com/dzeqeo9b3/image/upload/v1505191447/icon-comment_mg6xzj.png" />
+      </div>
     )
   }
 }
